bdba050db8499e24118fc54d7e31227ffc0e8365
Aditinair2 <aditi7493@gmail.com>
2017-07-01 15:11:34 +0530

    new project

---
+++ backend_project2/src/main/java/com/niit/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.niit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Configuration
+@EnableWebMvc   //<mvc:annotation-driven>
+@ComponentScan(basePackages="com.niit")  //<context:component-scan>
+
+public class WebConfig {
+	/*
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver internalResourceViewResolver= new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/pages/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+		
+	}
+	*/
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/resources/**")
+		.addResourceLocations("/WEB-INF/resources/");
+	}
+}
+
+
